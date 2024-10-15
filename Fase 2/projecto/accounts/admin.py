2a9b45edba9b22cab732d7117cf4a8baea15615f
Joaquin Onate <joa.onate@duocuc.cl>
2024-10-14 21:41:18 -0300

    Roles y cursos (sin formulario todavia)

--- Fase 2/projecto/accounts/admin.py
+++ Fase 2/projecto/accounts/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Personas
+from .models import Personas, Perfiles,PersonasPerfiles, Cursos
 
 # Register your models here.
 admin.site.register(Personas)
+admin.site.register(Perfiles)
+admin.site.register(PersonasPerfiles)
+admin.site.register(Cursos)
+
+
--- Fase 2/projecto/accounts/models.py
+++ Fase 2/projecto/accounts/models.py
@@ -11,10 +11,46 @@ class Personas(models.Model):
 	PERS_TELEFONO= models.CharField(max_length=14,null=True, blank=True,verbose_name='Pers_telefono')
 	PERS_DIRECCION = models.CharField(max_length=40,null=True, blank=True,verbose_name='Pers_direccion')
 
-	
+
 
 	class Meta:
 		ordering = ['-id']
 	
 	def __str__(self):
-   		return self.PERS_RUT
+   		return self.PERS_NOMBRECOMPLETO
+
+
+class Perfiles(models.Model):
+	PERF_ID = models.AutoField(primary_key=True, verbose_name = 'Id de perfiles')
+	PERF_NOMBRE = models.CharField(max_length = 50, verbose_name = 'Perfiles')
+
+	def __str__(self):
+		return self.PERF_NOMBRE
+
+
+class Cursos(models.Model):
+	CURS_ID = models.AutoField(primary_key=True, verbose_name = 'Id de curso')
+	CURS_NOMBRE = models.CharField(max_length = 50, verbose_name = 'Nombre curso')
+	CURS_SALA = models.IntegerField(verbose_name = 'Numero de sala')
+	CURS_ANNO = models.IntegerField(verbose_name = 'Año de curso')
+
+
+	def __str__(self):
+		return self.CURS_NOMBRE
+
+
+class PersonasPerfiles(models.Model):
+	PEPE_ID = models.AutoField(primary_key=True)
+	PEPE_FECHACREACION = models.DateField(auto_now_add=True, verbose_name='Fecha de Creación')
+	PEPE_PERF_ID = models.ForeignKey(Perfiles, on_delete=models.CASCADE, related_name='personas_perfiles')
+	PEPE_PERS_ID = models.ForeignKey(Personas, on_delete=models.CASCADE, related_name='perfiles')
+	PEPE_CURS_ID = models.ForeignKey(Cursos, on_delete=models.CASCADE,null=True, related_name='cursos')
+
+	class Meta:
+		verbose_name = 'Perfil de Persona'
+		verbose_name_plural = 'Perfiles de Personas'
+		ordering = ['PEPE_ID']  
+	def __str__(self):
+		return f'Perfil {self.PEPE_PERF_ID.PERF_NOMBRE} de {self.PEPE_PERS_ID.PERS_NOMBRECOMPLETO} curso {self.PEPE_CURS_ID.CURS_NOMBRE}'
+
+
